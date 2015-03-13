d84fe693848b03f126190613fda5c955400aa70c
Mike Skalnik <mike.skalnik@gmail.com>
2015-03-13 14:27:24 -0700

    Try to actually write some JS :pencil:

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 (function() {
   function ready(fn) {
-    if (document.readyState != 'loading'){
+    if (document.readyState != "loading"){
       fn();
     } else {
-      document.addEventListener('DOMContentLoaded', fn);
+      document.addEventListener("DOMContentLoaded", fn);
     }
   }
 
   ready(function() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/beers.json', true);
+    request.open("GET", "beers.json", true);
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
-        var beers = JSON.parse(request.responseText);
-        console.log(beers);
+        var beers = JSON.parse(request.responseText)['beers'];
+
+        // First beer is the free space beer
+        document.querySelector('td#freespace').innerText = beers[0];
+        beers = beers.slice(1, beers.length)
+
+        // Populate each table element with a beer, using each one once before repeating
+        var beersUsed = []
+        var spaces = document.querySelectorAll('td')
+        for(var i = 0; i < spaces.length; i++) {
+          // If we've used all the beers, restart the list
+          if(beers.length < 1) {
+            beers = beersUsed;
+            beersUsed = [];
+          }
+
+          var chosenIndex = Math.random() * beers.length;
+          spaces[i].innerText = beers[chosenIndex];
+
+          // Add the beer to our used list and remove it from original list
+          beersUsed.concat(beers[chosenIndex]);
+          beers = beers.slice(0, chosenIndex).concat(beers.slice(chosenIndex + 1, beers.length));
+        }
       }
     };
+    request.send();
   });
 })();
