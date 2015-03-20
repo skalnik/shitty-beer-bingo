301286f2b01afaf6dc7b2fd1372891dc7cd51ea6
Mike Skalnik <mike.skalnik@gmail.com>
2015-03-19 23:30:33 -0700

    Create a BingoCard object & use it :black_joker:

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,16 @@
-(function() {
-  function ready(fn) {
-    if (document.readyState != "loading"){
-      fn();
-    } else {
-      document.addEventListener("DOMContentLoaded", fn);
+define(function(require) {
+  var BingoCard = require('BingoCard');
+  var card = null;
+  var request = new XMLHttpRequest();
+
+  request.open("GET", "beers.json", true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var beers = JSON.parse(request.responseText)['beers'];
+      card = BingoCard.generate(beers);
+      card.updateTable();
     }
-  }
+  };
 
-  ready(function() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "beers.json", true);
-
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        var beers = JSON.parse(request.responseText)['beers'];
-
-        // First beer is the free space beer
-        document.querySelector('td#freespace').innerText = beers[0];
-        beers = beers.slice(1, beers.length);
-
-        // Populate each table element with a beer, using each one once before repeating
-        var beersUsed = [];
-        var spaces = document.querySelectorAll('td:not(#freespace)');
-        for(var i = 0; i < spaces.length; i++) {
-          // If we've used all the beers, restart the list
-          if(beers.length < 1) {
-            beers = beersUsed.slice(0);
-            beersUsed = [];
-          }
-
-          var chosenIndex = Math.floor(Math.random() * beers.length);
-          spaces[i].innerText = beers[chosenIndex];
-
-          // Add the beer to our used list and remove it from original list
-          beersUsed = beersUsed.concat(beers[chosenIndex]);
-          beers = beers.slice(0, chosenIndex).concat(beers.slice(chosenIndex + 1, beers.length));
-        }
-      }
-    };
-    request.send();
-  });
-})();
+  request.send();
+});
---
+++ assets/js/bingoCard.js
@@ -0,0 +1,81 @@
+define('BingoCard', function() {
+  function BingoCard() {
+    this.board = [
+      ['Beer', 'Beer', 'Beer', 'Beer', 'Beer'],
+      ['Beer', 'Beer', 'Beer', 'Beer', 'Beer'],
+      ['Beer', 'Beer', 'Beer', 'Beer', 'Beer'],
+      ['Beer', 'Beer', 'Beer', 'Beer', 'Beer'],
+      ['Beer', 'Beer', 'Beer', 'Beer', 'Beer']
+    ];
+  }
+
+  BingoCard.prototype.freespace = function(beer) {
+    if(typeof beer != 'undefined') {
+      this.board[2][2] = beer;
+    }
+    else {
+      return this.board[2][2];
+    }
+  }
+
+  BingoCard.prototype.updateTable = function() {
+    this.eachSquare(function(row, col, beer) {
+      // Add 1 to the row to skip the title row
+      var tableRow = document.querySelectorAll('tr')[row + 1];
+      var square = tableRow.querySelectorAll('td')[col];
+      square.innerText = beer;
+    });
+  }
+
+  BingoCard.prototype.set = function(row, col, val) {
+    this.board[row][col] = val;
+  }
+
+  BingoCard.prototype.eachSquare = function(callback) {
+    var rows = this.board.length;
+    var cols = this.board[0].length;
+
+    for(var row = 0; row < rows; row++) {
+      for(var col = 0; col < cols; col++) {
+        callback(row, col, this.board[row][col]);
+      }
+    }
+  }
+
+  BingoCard.generate = function(beerList) {
+    card = new BingoCard();
+
+    // First beer is the free space beer
+    var freespace = beerList[0];
+    beerList = beerList.slice(1, beerList.length);
+
+    // Populate each table element with a beer, using each one once before repeating
+    var beersUsed = [];
+
+    card.eachSquare(function(row, col, element) {
+      if(beerList.length < 1) {
+        beerList = beersUsed.slice(0);
+        beersUsed = [];
+      }
+
+      var chosenIndex = Math.floor(Math.random() * beerList.length);
+      card.set(row, col, beerList[chosenIndex]);
+
+      // Add the beer to our used list and remove it from original list
+      beersUsed = beersUsed.concat(beerList[chosenIndex]);
+      beerList = beerList.slice(0, chosenIndex).
+                  concat(beerList.slice(chosenIndex + 1, beerList.length));
+    });
+
+    // Set the freespace last since we don't want overwrite in in `eachSquare`
+    card.freespace(freespace);
+
+    return card;
+  }
+
+  BingoCard.load = function(localStorage) {
+
+  }
+
+  return BingoCard;
+})
